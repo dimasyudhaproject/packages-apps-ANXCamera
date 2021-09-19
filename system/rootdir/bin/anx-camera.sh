831781958719279f1db35e01b97c197c962491c4
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2021-09-19 13:29:16 +0000

    Fix derp in shell script
    Signed-off-by: Dimas Yudha Pratama <fsdimasyudha@gmail.com>

--- system/rootdir/bin/anx-camera.sh
+++ system/rootdir/bin/anx-camera.sh
@@ -1,20 +1,17 @@
-#!/vendor/bin/sh
+#!/system/bin/sh
 
 SYSTEM_PATH="/system/etc"
 ANX_PATH="/sdcard/.ANXCamera"
 DEVICE_NAME=$(getprop ro.product.device)
 
 
-if [ -d $ANX_PATH ];
-    then
-        mkdir -p $ANX_PATH/cheatcodes
-	cp -rf $SYSTEM_PATH/ANXCamera/cheatcodes/cheatcodes.json $ANX_PATH/cheatcodes
-        mkdir -p $ANX_PATH/cheatcodes_reference
-	cp-rf $SYSTEM_PATH/ANXCamera/cheatcodes/cheatcodes.json $ANX_PATH/cheatcodes_reference
-        mkdir -p $ANX_PATH/features
-	cp -rf $SYSTEM_PATH/device_features/$DEVICE_NAME $ANX_PATH/features
-        mkdir -p $ANX_PATH/features_reference
-	cp -rf $SYSTEM_PATH/device_features/$DEVICE_NAME $ANX_PATH/features_reference
-fi
-
+rm -rf $ANX_PATH
+mkdir -p $ANX_PATH/cheatcodes
+cp -rf $SYSTEM_PATH/ANXCamera/cheatcodes/cheatcodes.json $ANX_PATH/cheatcodes
+mkdir -p $ANX_PATH/cheatcodes_reference
+cp -rf $SYSTEM_PATH/ANXCamera/cheatcodes/cheatcodes.json $ANX_PATH/cheatcodes_reference
+mkdir -p $ANX_PATH/features
+cp -rf $SYSTEM_PATH/device_features/${DEVICE_NAME}* $ANX_PATH/features
+mkdir -p $ANX_PATH/features_reference
+cp -rf $SYSTEM_PATH/device_features/${DEVICE_NAME}* $ANX_PATH/features_reference
 mkdir -p /sdcard/DCIM/Camera/Lab_options_visible
