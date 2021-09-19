b0c65d484cdc29954a53a20fa5f831c181e68135
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2021-09-19 08:02:39 +0000

    Take only needed file instead
    Signed-off-by: Dimas Yudha Pratama <fsdimasyudha@gmail.com>

--- system/rootdir/bin/anx-camera.sh
+++ system/rootdir/bin/anx-camera.sh
@@ -1,14 +1,20 @@
-#!/system/bin/sh
+#!/vendor/bin/sh
 
 SYSTEM_PATH="/system/etc"
 ANX_PATH="/sdcard/.ANXCamera"
+DEVICE_NAME=$(getprop ro.product.device)
 
-if [ -d "$ANX_PATH" ];
+
+if [ -d $ANX_PATH ];
     then
-        mkdir -p $ANX_PATH/cheatcodes && cp -rf $SYSTEM_PATH/ANXCamera/cheatcodes/* $ANX_PATH/cheatcodes
-        mkdir -p $ANX_PATH/cheatcodes_reference && cp-rf $SYSTEM_PATH/ANXCamera/cheatcodes/* $ANX_PATH/cheatcodes_reference
-        mkdir -p $ANX_PATH/features && cp -rf $SYSTEM_PATH/device_features/* $ANX_PATH/features
-        mkdir -p $ANX_PATH/features_reference && cp -rf $SYSTEM_PATH/device_features/* $ANX_PATH/features_reference
+        mkdir -p $ANX_PATH/cheatcodes
+	cp -rf $SYSTEM_PATH/ANXCamera/cheatcodes/cheatcodes.json $ANX_PATH/cheatcodes
+        mkdir -p $ANX_PATH/cheatcodes_reference
+	cp-rf $SYSTEM_PATH/ANXCamera/cheatcodes/cheatcodes.json $ANX_PATH/cheatcodes_reference
+        mkdir -p $ANX_PATH/features
+	cp -rf $SYSTEM_PATH/device_features/$DEVICE_NAME $ANX_PATH/features
+        mkdir -p $ANX_PATH/features_reference
+	cp -rf $SYSTEM_PATH/device_features/$DEVICE_NAME $ANX_PATH/features_reference
 fi
 
 mkdir -p /sdcard/DCIM/Camera/Lab_options_visible
